f6850a41ea5cf51212303513b05d68e5a9e0ee2f
Valentin Bertaud <vabertau@paul-f3Ar1s1.clusters.42paris.fr>
2024-05-29 16:05:05 +0200

    fix on invalid read fo echo 0

--- srcs/expands/expands.c
+++ srcs/expands/expands.c
@@ -6,7 +6,7 @@
 /*   By: vabertau <vabertau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 18:46:03 by vabertau          #+#    #+#             */
-/*   Updated: 2024/05/12 11:43:34 by vabertau         ###   ########.fr       */
+/*   Updated: 2024/05/29 16:00:06 by vabertau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -151,6 +151,8 @@ void	launch_expand(t_data *data, t_token *token)
 			replace_var(data, &word, i, len_var);
 			i += len_var;
 		}
+		if (!word[i])
+			break ;
 		i++;
 	}
 	token->word = word;
