87a8304cabfda11c33493d136e981c8537a76460
Valentin Bertaud <vabertau@paul-f3Ar3s11.clusters.42paris.fr>
2024-06-03 18:53:13 +0200

    changed Parsing error for syntax error

--- srcs/parsing/parsing_error.c
+++ srcs/parsing/parsing_error.c
@@ -6,7 +6,7 @@
 /*   By: vabertau <vabertau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 11:46:55 by vabertau          #+#    #+#             */
-/*   Updated: 2024/05/09 14:46:36 by vabertau         ###   ########.fr       */
+/*   Updated: 2024/06/03 18:50:35 by vabertau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	parsing_error(t_data *data)
 {
-	ft_printf("Parsing error\n");
+	ft_printf("syntax error\n");
 	set_exitloop_free(data);
 	data->last_return_code = 1;
 }
